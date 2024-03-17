eb4f5b5b18e49e8c7ed2123ad010b3805eaa13b4
Akshit Trikha <akshittrikha01@gmail.com>
2024-03-17 10:27:50 +0530

    Add: ShortenURL function that returns the shortened URL Add: Helper functions for shortener.go

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -4,6 +4,10 @@ package routes
 
 import (
 	"time"
+
+	"github.com/akshtrikha/url-shortener-golang/api/helpers"
+	"github.com/gofiber/fiber/v2"
+	"github.com/asaskevich/govalidator"
 )
 
 // ? Defines the structure of a request
@@ -20,4 +24,31 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+// ShortenURL function parses the payload and returns the shortened URL
+func ShortenURL(ctx *fiber.Ctx) error {
+
+	body := new(request)
+
+	if err := ctx.BodyParser(&body); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse the JSON payload"})
+	}
+
+	//TODO: implement rate limiting
+
+	//? validate URL
+	if !govalidator.IsURL(body.URL) {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	//? check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return ctx.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Invalid domain"})
+	}
+
+	//? enforce HTTPS
+	body.URL = helpers.EnforceHTTP(body.URL)
+
+	return nil
 }
